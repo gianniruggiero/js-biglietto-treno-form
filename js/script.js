af4a2652bd11ac5ee8128c2d59dfb13e9ed72f7e
GIanni Ruggiero <gianni@groovecompany.it>
2020-08-20 17:09:47 +0200

    aggiunto: animazioni su click btn Genera e Annulla; check dati inseriti

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ genera.addEventListener ("click",
   function() {
     // Prendo il nome inserito
     var nome = document.getElementById("nome").value;
-    console.log(nome);
+    console.log("nome: " + nome);
     // Prendo i km da percorrere inseriti dall'utente
     var kmViaggio = document.getElementById("km").value;
     console.log(kmViaggio);
@@ -14,35 +14,62 @@ genera.addEventListener ("click",
     var fasciaEta = document.getElementById("fascia_eta").value;
     console.log(fasciaEta);
 
-    // Calcolo il prezzo totale del biglietto
-    var costoKm = 0.21;
-    var costoTot = (kmViaggio * costoKm);
-    console.log("Totale: " + costoTot);
-    var offerta = "Biglietto Standard";
-
-    // Verifico la fascia d'età scelta e calcolo relativo sconto
-    if (fasciaEta == "minorenne") {
-      costoTot = costoTot * 0.8;
-      offerta = "Sconto Minorenne"
-    } else if (fasciaEta == "over65") {
-      costoTot = costoTot * 0.6;
-      offerta = "Sconto Silver"
+    var erroreInput = 0;
+    // verifica se è stato inserito il numero di Km
+    if (kmViaggio == 0) {
+      erroreInput = 1;
+    }
+    // verifica se è stato inserito il nome
+    if (nome == "") {
+      erroreInput = 2;
     }
 
-    // Arrotondamento costo Totale
-    costoTot = costoTot.toFixed(2);
+    console.log("kmViaggio: " + kmViaggio);
+    console.log("erroreInput: " + erroreInput);
 
-    // Genero numero (random) per carrozza e codice CP
-    var numCarrozza = Math.floor(Math.random() * 9) + 1;
-    var codiceCP = Math.floor(Math.random() * 9999);
+    if (erroreInput == 0) {
+      // Calcolo il prezzo totale del biglietto
+      var costoKm = 0.21;
+      var costoTot = (kmViaggio * costoKm);
+      console.log("Totale: " + costoTot);
+      var offerta = "Biglietto Standard";
 
-    // Inserisco i dati calcolati nel biglietto
-    document.getElementById("nome_ins").innerHTML = nome;
-    document.getElementById("costo").innerHTML = costoTot + " €";
-    document.getElementById("offerta").innerHTML = offerta;
-    document.getElementById("carrozza").innerHTML = numCarrozza;
-    document.getElementById("codice_cp").innerHTML = "9" + codiceCP;
-    document.getElementById("tkt_det").classList.add("show");
+      // Verifico la fascia d'età scelta e calcolo relativo sconto
+      if (fasciaEta == "minorenne") {
+        costoTot = costoTot * 0.8;
+        offerta = "Sconto Minorenne"
+      } else if (fasciaEta == "over65") {
+        costoTot = costoTot * 0.6;
+        offerta = "Sconto Silver"
+      }
+
+      // Arrotondamento costo Totale
+      costoTot = costoTot.toFixed(2);
+
+      // Genero numero (random) per carrozza e codice CP
+      var numCarrozza = Math.floor(Math.random() * 9) + 1;
+      var codiceCP = Math.floor(Math.random() * 9999);
+
+      // Inserisco i dati calcolati nel biglietto
+      document.getElementById("nome_ins").innerHTML = nome;
+      document.getElementById("costo").innerHTML = costoTot + " €";
+      document.getElementById("offerta").innerHTML = offerta;
+      document.getElementById("carrozza").innerHTML = numCarrozza;
+      document.getElementById("codice_cp").innerHTML = "9" + codiceCP;
+      document.getElementById("tkt_det").classList.add("show");
+      document.getElementById("tkt_det").classList.remove("click_annulla_anim");
+      document.getElementById("tkt_det").classList.add("click_genera_anim");
+    } else if (erroreInput == 1) {
+      // non è stato inserito numero di km
+      alert ("Attenzione: inserire Km da percorrere")
+      erroreInput = 0;
+      // document.getElementById("km").style.background = "yellow";
+      // document.getElementById("km").style.border = "2px solid black";
+    } else {
+      // non è stato inserito numero di km
+      alert ("Attenzione: inserire il nome del passeggero")
+      erroreInput = 0;
+    }
   }
 )
 
@@ -51,10 +78,12 @@ var annulla = document.getElementById('btn_annulla');
 annulla.addEventListener ("click",
   function () {
     // fa scomparire i dettagli del biglietto
+    document.getElementById("tkt_det").classList.add("click_annulla_anim");
     document.getElementById('tkt_det').classList.remove("show");
+    document.getElementById("tkt_det").classList.remove("click_genera_anim");
     // resettare i campi dati_input
     document.getElementById('nome').value = "";
     document.getElementById('km').value = "";
-    document.getElementById('fascia_eta').value = "";
+    document.getElementById('fascia_eta').value = "maggiorenne";
   }
 )
